c199f5341ba96cb5b5d74ac974378483f25aab8d
IsmaCortGtz <ismacortgtz@gmail.com>
2024-07-11 12:04:29 -0600

    Title updated

--- views/singup.php
+++ views/singup.php
@@ -11,7 +11,7 @@ $num2 = rand(1, 10);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Los caninos</title>
+  <title>Crear cuenta</title>
   <link rel="shortcut icon" href="/src/images/logo.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="/src/styles/button.css">
