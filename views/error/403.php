25b2b17fef202559ca53c17ec756861c0449b456
2123100562 <2123100562@soy.utj.edu.mx>
2024-07-26 14:00:05 +0000

    Errors 404 and 403 added

--- views/error/403.php
+++ views/error/403.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Error 404</title>
+  <title>Error 403</title>
   <link rel="shortcut icon" href="/src/images/logo.png">
   <link rel="stylesheet" href="/src/styles/index.css">
   <link rel="stylesheet" href="/src/styles/landing.css">
@@ -15,9 +15,9 @@
   <?php require(__DIR__ . "/../../components/header.php"); ?>
 
   <main class="d-flex flex-column gap-3 justify-content-center align-items-center">
-    <img src="/src/images/error/Error404.svg" width="50%" style="max-width: 300px;" alt="Error404">
-    <h2 class="fw-bold">Página no encontrada</h2>
-    <p>La página que estás buscando no existe.</p>
+    <img src="/src/images/error/Error403.svg" width="50%" style="max-width: 300px;" alt="Error403">
+    <h2 class="fw-bold">Acceso denegado</h2>
+    <p>No tienes los permisos necesarios para acceder a esta página.</p>
     <a class="btn btn-orange-primary" href="/">Volver al inicio</a>
   </main>
 
